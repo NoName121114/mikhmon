7a601d7a0da68380f5763634c3198c02d4429f2c
NoName121114 <92351544+NoName121114@users.noreply.github.com>
2024-11-09 06:58:12 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,5 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['SUKADANA'] = array ('1'=>'SUKADANA!chr.yoowifi.site:7557','SUKADANA@|@NoName','SUKADANA#|#aWNjaQ==','SUKADANA%Sukadana Hotspot','SUKADANA^sukadana.id','SUKADANA&Rp','SUKADANA*10','SUKADANA(1','SUKADANA)','SUKADANA=10','SUKADANA@!@disable');
